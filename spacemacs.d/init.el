40b91da2cc43217223ef1496f08dc16e447b0a2d
Teofil Camarasu <teofilcamarasu@gmail.com>
2018-10-14 15:34:20 +0100

    Set scratch to org-mode; disable debug errors

--- spacemacs.d/init.el
+++ spacemacs.d/init.el
@@ -132,7 +132,7 @@ values."
    ;; True if the home buffer should respond to resize events.
    dotspacemacs-startup-buffer-responsive t
    ;; Default major mode of the scratch buffer (default `text-mode')
-   dotspacemacs-scratch-mode 'text-mode
+   dotspacemacs-scratch-mode 'org-mode
    ;; List of themes, the first of the list is loaded when spacemacs starts.
    ;; Press <SPC> T n to cycle to the next theme in the list (works great
    ;; with 2 themes variants, one dark and one light)
@@ -320,7 +320,7 @@ layers configuration.
 This is the place where most of your configurations should be done. Unless it is
 explicitly specified that a variable should be set before a package is loaded,
 you should place your code here."
-  (setq debug-on-error t)
+  ;;(setq debug-on-error t)
   (org-babel-load-file "~/.spacemacs.d/emacs.org")
   )
 
