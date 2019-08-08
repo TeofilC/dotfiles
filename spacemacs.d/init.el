3fa952618b15f2a310dcb82d3ebe937d54fdf717
Teofil Camarasu <teofilcamarasu@gmail.com>
2019-08-09 00:08:21 +0100

    Make leuven default emacs theme

--- spacemacs.d/init.el
+++ spacemacs.d/init.el
@@ -136,7 +136,8 @@ values."
    ;; List of themes, the first of the list is loaded when spacemacs starts.
    ;; Press <SPC> T n to cycle to the next theme in the list (works great
    ;; with 2 themes variants, one dark and one light)
-   dotspacemacs-themes '(tsdh-dark
+   dotspacemacs-themes '(leuven
+                         tsdh-dark
                          spacemacs-dark
                          spacemacs-light)
    ;; If non nil the cursor color matches the state color in GUI Emacs.
